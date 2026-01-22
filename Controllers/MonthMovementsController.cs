f0ac9113b2fcd798915556852f0ab0eea6de6a88
Dieimp <mraingue@gmail.com>
2026-01-21 23:28:59 -0300

    finised create monthMovement

--- Controllers/MonthMovementsController.cs
+++ Controllers/MonthMovementsController.cs
@@ -22,6 +22,7 @@ namespace NoWasteOfMoney.Controllers
         }
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<PagedResult<MonthMovement>>> GetMonthMovements(
             [FromQuery] int pageNumber = 1,
             [FromQuery] int pageSize = 10,
@@ -29,6 +30,48 @@ namespace NoWasteOfMoney.Controllers
         {
             var pagedResult = await _service.GetAll(pageNumber, pageSize, personId);
             return Ok(pagedResult);
+        }
+        [HttpGet("{personId},{date}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<PagedResult<MonthMovement>>> GetMonthMovementsByMonth(
+            [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 10,
+            [FromQuery] int personId = 0,
+            [FromQuery] DateOnly? date = null
+            )
+
+        {
+            DateOnly refDate = date ?? DateOnly.FromDateTime(DateTime.UtcNow);
+            var pagedResult = await _service.GetByMonth(pageNumber, pageSize, personId, refDate);
+            if (pagedResult == null)
+            {
+                return NotFound($"Pessoa com ID {personId} não encontrada.");
+
+            }
+            return Ok(pagedResult);
+        }
+
+        [HttpPost("{personId}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<MonthMovement>> Create(int personId, CreateMonthMovement req)
+        {
+
+            var monthMovement = new MonthMovement
+            {
+                MovementId = req.MovementId,
+                PersonId = req.PersonId,
+                Year = req.Date.Year,
+                Month = req.Date.Month,
+                Value = req.Value,
+
+            };
+
+            await _service.Create(personId, monthMovement);
+
+            return Ok(monthMovement.Id);
         }
 
 
